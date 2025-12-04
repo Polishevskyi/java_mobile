fbb9135d8d336923a2785b75ee40bd7cecbf10a0
Serhii Polishevskyi <s.polishevskiy@gmail.com>
2025-12-04 12:57:54 +0200

    Implement retry mechanism for flaky tests using TestNG IRetryAnalyzer

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,10 @@
 package tests;
 
 import io.qameta.allure.testng.AllureTestNg;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
+import utils.RetryAnalyzer;
 import utils.appium.TestConfig;
 import utils.appium.driver.AppFactory;
 import utils.appium.driver.AppiumServerManager;
@@ -10,9 +12,16 @@ import utils.listeners.AllureListener;
 import utils.listeners.BrowserStackListener;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 @Listeners({AllureTestNg.class, AllureListener.class, BrowserStackListener.class})
 public class BaseTest {
+
+    @BeforeTest
+    public void setUpRetry(ITestContext context) {
+        Arrays.stream(context.getAllTestMethods())
+                .forEach(method -> method.setRetryAnalyzerClass(RetryAnalyzer.class));
+    }
 
     @BeforeSuite
     public static void serverStart() {
